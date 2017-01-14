689556d25ba63ebfa6cfdb8814032e3e5c9ad472
Jonathan Lam <lamj18@uci.edu>
2017-01-14 03:11:38 -0800

    removed Pods

--- Pods/Target Support Files/Pods-MD5HashTests/Pods-MD5HashTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MD5HashTests : NSObject
-@end
-@implementation PodsDummy_Pods_MD5HashTests
-@end
--- Pods/Target Support Files/Pods-MD5HashUITests/Pods-MD5HashUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MD5HashUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MD5HashUITests
-@end
